e63765729b2ea2df48f76201ee7077857b670d5a
anastasyaO <anastasyaokvitasari34@gmail.com>
2023-09-08 14:30:00 +0700

    membuat portopolio

---
+++ img/index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>My portfolio | Anastasya</title>
+</head>
+<body id="home">
+    
+
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger shadow-sm fixed-top">
+    <navbar-fixed> </navbar-fixed>
+  <div class="container">
+      <a class="navbar-brand" href="#">Anastasya</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#About">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#Projects">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#Contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+<!--Akhir Navbar-->
+
+<!--jumbotron-->
+<section class="jumbotron text-center">
+    <img src="img/logoo.jpg" alt="Anastasya" width="300" 
+    class="rounded-circle img-thumbnail"/>
+    <h1 class="display-4">Anastasya</h1>
+    <p class="lead">Graphic Artist-Web Designer-Ilustrator</p>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f61010" fill-opacity="1" d="M0,128L18.5,112C36.9,96,74,64,111,90.7C147.7,117,185,203,222,197.3C258.5,192,295,96,332,90.7C369.2,85,406,171,443,197.3C480,224,517,192,554,181.3C590.8,171,628,181,665,208C701.5,235,738,277,775,256C812.3,235,849,149,886,138.7C923.1,128,960,192,997,224C1033.8,256,1071,256,1108,240C1144.6,224,1182,192,1218,170.7C1255.4,149,1292,139,1329,117.3C1366.2,96,1403,64,1422,48L1440,32L1440,320L1421.5,320C1403.1,320,1366,320,1329,320C1292.3,320,1255,320,1218,320C1181.5,320,1145,320,1108,320C1070.8,320,1034,320,997,320C960,320,923,320,886,320C849.2,320,812,320,775,320C738.5,320,702,320,665,320C627.7,320,591,320,554,320C516.9,320,480,320,443,320C406.2,320,369,320,332,320C295.4,320,258,320,222,320C184.6,320,148,320,111,320C73.8,320,37,320,18,320L0,320Z"></path></svg>
+</section>
+
+<!--akhir jumbotron-->
+<section id="About">
+<div class="container">
+  <div class="row text-center mb-3">
+    <div class="col">
+      <h2>About Me</h2>
+    </div>
+  </div>
+  <div class="row justify-content-center fs-5 text-center">
+    <div class="col-md-4">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt voluptatibus consectetur qui mollitia rerum accusamus quibusdam? Enim ratione consequuntur saepe.</p>
+    </div>
+    <div class="col-md-4">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta inventore! Voluptates et illo alias voluptas temporibus impedit aut, enim rem corrupti tempora adipisci nam.</p>
+    </div>
+    <div class="col-md-4">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, soluta inventore! Voluptates et illo alias voluptas temporibus impedit aut, enim rem corrupti tempora adipisci nam.</p>
+    </div>
+</div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f61010" fill-opacity="1" d="M0,128L18.5,112C36.9,96,74,64,111,90.7C147.7,117,185,203,222,197.3C258.5,192,295,96,332,90.7C369.2,85,406,171,443,197.3C480,224,517,192,554,181.3C590.8,171,628,181,665,208C701.5,235,738,277,775,256C812.3,235,849,149,886,138.7C923.1,128,960,192,997,224C1033.8,256,1071,256,1108,240C1144.6,224,1182,192,1218,170.7C1255.4,149,1292,139,1329,117.3C1366.2,96,1403,64,1422,48L1440,32L1440,320L1421.5,320C1403.1,320,1366,320,1329,320C1292.3,320,1255,320,1218,320C1181.5,320,1145,320,1108,320C1070.8,320,1034,320,997,320C960,320,923,320,886,320C849.2,320,812,320,775,320C738.5,320,702,320,665,320C627.7,320,591,320,554,320C516.9,320,480,320,443,320C406.2,320,369,320,332,320C295.4,320,258,320,222,320C184.6,320,148,320,111,320C73.8,320,37,320,18,320L0,320Z"></path></svg>
+</selection>
+
+<!--About-->
+
+<!--Akhir About-->
+
+
+
+
+<!--projects-->
+
+<section id="Projects">
+  <div class="container">
+    <div class="row text-center mb-3">
+      <div class="col">
+        <h2>My Projects</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <img src="img/aanime.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <img src="img/aanime1.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <img src="img/aanime6.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <img src="img/aanime3.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <img src="img/aanime4.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 mb-3">
+        <div class="card">
+          <img src="img/aanime5.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f61010" fill-opacity="1" d="M0,128L18.5,112C36.9,96,74,64,111,90.7C147.7,117,185,203,222,197.3C258.5,192,295,96,332,90.7C369.2,85,406,171,443,197.3C480,224,517,192,554,181.3C590.8,171,628,181,665,208C701.5,235,738,277,775,256C812.3,235,849,149,886,138.7C923.1,128,960,192,997,224C1033.8,256,1071,256,1108,240C1144.6,224,1182,192,1218,170.7C1255.4,149,1292,139,1329,117.3C1366.2,96,1403,64,1422,48L1440,32L1440,320L1421.5,320C1403.1,320,1366,320,1329,320C1292.3,320,1255,320,1218,320C1181.5,320,1145,320,1108,320C1070.8,320,1034,320,997,320C960,320,923,320,886,320C849.2,320,812,320,775,320C738.5,320,702,320,665,320C627.7,320,591,320,554,320C516.9,320,480,320,443,320C406.2,320,369,320,332,320C295.4,320,258,320,222,320C184.6,320,148,320,111,320C73.8,320,37,320,18,320L0,320Z"></path></svg>
+</section>
+
+
+
+<!--akhir projects-->
+
+<!--contact-->
+<section id="Contact">
+<div class="container">
+  <div class="row text-center">
+    <div class="col">
+      <h2>Contact Me</h2>
+
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-md-4">
+
+    </div >
+    <form>
+      <div class="mb-3">
+        <label for="Name" class="form-label">Nama Lengkap</label>
+        <input type="text" class="form-control" id="name" aria-describedby="name">
+        
+      </div>
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" id="Email" aria-describedby="email">
+        
+      </div>
+      <div class="mb-3">
+        <label for="pesan" class="form-label">Pesan</label>
+        <textarea class="form-control" id="pesan" rows="3"></textarea>
+      </div>
+      
+      <button type="submit" class="btn bg-danger">Kirim</button>
+    </form>
+  </div>
+</div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f61010" fill-opacity="1" d="M0,128L18.5,112C36.9,96,74,64,111,90.7C147.7,117,185,203,222,197.3C258.5,192,295,96,332,90.7C369.2,85,406,171,443,197.3C480,224,517,192,554,181.3C590.8,171,628,181,665,208C701.5,235,738,277,775,256C812.3,235,849,149,886,138.7C923.1,128,960,192,997,224C1033.8,256,1071,256,1108,240C1144.6,224,1182,192,1218,170.7C1255.4,149,1292,139,1329,117.3C1366.2,96,1403,64,1422,48L1440,32L1440,320L1421.5,320C1403.1,320,1366,320,1329,320C1292.3,320,1255,320,1218,320C1181.5,320,1145,320,1108,320C1070.8,320,1034,320,997,320C960,320,923,320,886,320C849.2,320,812,320,775,320C738.5,320,702,320,665,320C627.7,320,591,320,554,320C516.9,320,480,320,443,320C406.2,320,369,320,332,320C295.4,320,258,320,222,320C184.6,320,148,320,111,320C73.8,320,37,320,18,320L0,320Z"></path></svg>
+</path></svg></section>
+<!--akhir contact-->
+
+<!--FOOTER-->
+<footer class="bg-danger text-white text-center p-5">
+  <p>Created with <i class="bi bi-heart-fill text-danger "></i> by <a href="Anastasya" class="
+    text-white font-weight-bold">Anastasya</a></div></p>
+  
+</footer>
+<!-- AKHIR FOOTER-->
+    
+    </head>
+    <body>
+        
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" />
+
+<!--booptrap css-->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
+
+        <!--my css-->
+<link rel="stylesheet" href="style.css">
+  </body>
+</html>
